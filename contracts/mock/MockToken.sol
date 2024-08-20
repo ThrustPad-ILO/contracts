22625172b7cc4fef7a448e0ab9eb1fdadbfc4f47
new dapp sync <oluwafemialofe@yahoo.com>
2024-08-20 16:47:25 +0400

    Completed test for ThrustPadFactory Locker

---
+++ contracts/mock/MockToken.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.8.24;
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockToken is ERC20 {
+    constructor() ERC20("ERC20", "MOCK") {
+        _mint(msg.sender, 1000000000 * 10 ** decimals());
+    }
+}
---
+++ test/lock_factory.test.js
@@ -0,0 +1,87 @@
+const { expect, assert, should } = require("chai");
+const { ethers } = require("hardhat");
+const { time } = require("@nomicfoundation/hardhat-network-helpers");
+
+const twenty4Hours = 86400;
+
+describe("Locker Factory", function () {
+    before(async function () {
+        const [deployer] = await ethers.getSigners();
+        const ThrustpadLockerFactory = await ethers.getContractFactory("ThrustpadLockerFactory");
+        const factory = await ThrustpadLockerFactory.deploy();
+
+        await factory.waitForDeployment();
+
+        this.factory = factory;
+        this.factoryAddress = await factory.getAddress();
+        this.deployer = deployer;
+        this.deployerAddress = await deployer.getAddress();
+    });
+
+    describe("Locker", function () {
+        it("Should deploy token with known address", async function () {
+            const amount = ethers.parseEther("1000000000");
+            const MockToken = await ethers.getContractFactory("MockToken");
+            const token = await MockToken.deploy();
+            const tokenAddress = await token.getAddress();
+            const byteCode = await this.factory.getBytecode(tokenAddress, twenty4Hours, amount);
+
+            const salt = await this.factory.getdeployedLocksLen(this.deployerAddress);
+            const address = await this.factory.getAddressCreate2(byteCode, salt);
+
+            //Allow factory  to spend tokens
+            await token.approve(this.factoryAddress, amount);
+            await this.factory.newLock(tokenAddress, twenty4Hours, amount);
+
+            const deployedLocks = await this.factory.getdeployedLocks(this.deployerAddress);
+            const lock = await ethers.getContractAt("ThrustpadLocker", address);
+
+            assert.equal(address, deployedLocks[0]);
+            assert.equal(deployedLocks.length, 1);
+
+            await expect(lock.release()).to.be.revertedWith("TokenLock: lock time not expired");
+
+            assert.equal(await lock.token(), tokenAddress);
+            assert.equal(await lock.lockTime(), twenty4Hours);
+            assert.equal(await token.balanceOf(address), amount);
+        });
+
+        it("Should withdraw tokens after lock time", async function () {
+            const amount = ethers.parseEther("1000000000");
+            const MockToken = await ethers.getContractFactory("MockToken");
+            const token = await MockToken.deploy();
+            const tokenAddress = await token.getAddress();
+            const byteCode = await this.factory.getBytecode(tokenAddress, twenty4Hours, amount);
+
+            const salt = await this.factory.getdeployedLocksLen(this.deployerAddress);
+            const address = await this.factory.getAddressCreate2(byteCode, salt);
+
+            console.table({ address, tokenAddress });
+
+            //Allow factory  to spend tokens
+            await token.approve(this.factoryAddress, amount);
+            await this.factory.newLock(tokenAddress, twenty4Hours, amount);
+
+            const deployedLocks = await this.factory.getdeployedLocks(this.deployerAddress);
+
+            const lock = await ethers.getContractAt("ThrustpadLocker", address);
+
+            assert.equal(address, deployedLocks[1]);
+            assert.equal(deployedLocks.length, 2);
+
+            await time.increase(twenty4Hours);
+
+            assert.equal(await token.balanceOf(this.deployerAddress), 0);
+            assert.equal(await token.balanceOf(address), amount);
+
+            await expect(lock.release()).to.not.be.reverted;
+
+            assert.equal(await token.balanceOf(this.deployerAddress), amount);
+            assert.equal(await token.balanceOf(address), 0);
+
+            await expect(lock.release()).to.be.revertedWith("TokenLock: no tokens to release");
+        });
+    });
+});
+
+//For testing: npx hardhat test test/lock_factory.test.js --network hardhat
